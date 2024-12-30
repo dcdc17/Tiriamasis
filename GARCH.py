18daa122c3b12dc7e824697e2d0226a9b118b624
Gintaras S <gintaras10x@gmail.com>
2024-12-30 19:11:36 +0200

    Užbaigtas GARCH modelio kodas (`GARCH.py`)

--- GARCH.py
+++ GARCH.py
@@ -1,51 +1,54 @@
-import pandas as pd
-import numpy as np
+import os
 import warnings
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
-from tqdm import tqdm
-from arch import arch_model
+
 import matplotlib.dates as mdates
-from constants import tickers, metals, metal_pairs
-ts_order = {'^GSPC': (2, 1, 2),
-            '^DJI': (3, 1, 3),
-            '^IXIC': (2, 1, 2),
-            '^FTSE': (0, 1, 0),
-            '^GDAXI': (2, 1, 2),
-            '^FCHI': (0, 1, 0),
-            '^N100': (0, 1, 0),
-            'EURUSD=X': (0, 1, 0),
-            '^HSI': (0, 1, 0),
-            '^DXS': (2, 1, 4),
-            'GD=F': (2, 1, 1),
-            'EURRUB=X': (1, 1, 3),
-            'GC=F': (3, 1, 3),
-            'SI=F': (2, 1, 0),
-            'PL=F': (0, 1, 0),
-            'PA=F': (2, 1, 1),
-            'HG=F': (0, 1, 0),
-            'ALI=F': (0, 1, 1)}
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from arch import arch_model
+from statsmodels.graphics.tsaplots import plot_pacf
+from tqdm import tqdm
+
+from constants import tickers, metals
+
 warnings.simplefilter('ignore')
 
+BASE = 'all'
+os.makedirs(BASE, exist_ok=True)
+os.makedirs(os.path.join(BASE,'GARCH'), exist_ok=True)
+
+GARCH_PARAMS = {"p":1, "q":1, "mean":'constant', "vol":'GARCH', "dist":'normal'}
+
 # Load and scale data
-df = pd.read_csv('all.csv', index_col=0)
-scaler = MinMaxScaler(feature_range=(1, 2))
-df_all = pd.DataFrame(scaler.fit_transform(df), columns=df.columns, index=df.index)
+df = pd.read_csv(f'{BASE}.csv', index_col=0)
+df_all = df
 df_all.index = pd.to_datetime(df_all.index)
 df_all = df.iloc[::-1]
 df_stocks = df_all[tickers]
 df_metals = df_all[metals]
 
 
-def evaluate_model(ind, data, ax, forecast_period=30):
-    log_returns = np.log(data / data.shift(1))
-    log_returns = log_returns.dropna()
+def analyze_pacf():
+    fig, axs = plt.subplots(3, 2, figsize=(16, 9))
+    # Loop over metals to generate PACF subplots
+    for m, ax in tqdm(zip(metals, axs.flatten())):
+        plot_pacf(df[m], ax=ax)
+        ax.set_title(m)
+    plt.suptitle('Metalų rinkų PACF grafikai')
+    plt.tight_layout()
+    plt.savefig(os.path.join(BASE, 'GARCH', 'pacf.png'))
+    plt.show()
+
+
+def evaluate_model(data, ax, ax2, forecast_period=60):
+    log_returns = 100 * data.pct_change()
+    log_returns.dropna(inplace=True)
 
     # Fit GARCH model
-    am = arch_model(log_returns, mean='Zero', vol='GARCH', p=ts_order[ind][0] if ts_order[ind][0]>0 else 1, q=ts_order[ind][-1] if ts_order[ind][-1]>0 else 1, rescale=True)
-    res = am.fit(disp='final')
-    future_m = res.forecast(start=log_returns[-1], horizon=forecast_period)
-    future = future_m.residual_variance.iloc[-1, :]
+    am = arch_model(log_returns, **GARCH_PARAMS)
+    res = am.fit(disp='off')
+    future_m = res.forecast(horizon=forecast_period)
+    future = np.sqrt(future_m.variance.values[-1, :])
     # Calculate AIC and BIC
     aic = res.aic
     bic = res.bic
@@ -58,63 +61,87 @@ def evaluate_model(ind, data, ax, forecast_period=30):
     # Out-of-sample testing
     data_length = len(log_returns)
     train_size = int(0.8 * data_length)
+    test_size = data_length - train_size
     train_data = log_returns[:train_size]
     test_data = log_returns[train_size:]
-
-    res_oos = am.fit(last_obs=train_data[-1], disp='final')
-    forecast = res_oos.forecast(start=train_data[-1], horizon=len(test_data))
-
+    res_oos = am.fit(last_obs=train_data[-1], disp='off')
+    forecast = res_oos.forecast(horizon=len(test_data))
     # Calculate out-of-sample forecast error
-    forecast_vol = forecast.residual_variance.iloc[-1, :]
-    error = (test_data - forecast_vol).dropna()
+    forecast_vol = forecast.residual_variance.values[-1, :]
+    error = test_data - forecast_vol
 
-    # Plot actual log returns and forecasted data
-    ax.plot(mdates.date2num(test_data.index.tolist()), test_data, color='blue', label='Actual Log Returns')
-    ax.set_ylabel('Log Return')
-    ax.xaxis.set_major_locator(mdates.YearLocator())
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+    # PLOT 1: Predictability on test data: whole and rolling
+    rolling_predictions = []
+    for i in range(test_size):
+        train = log_returns[:-(test_size - i)]
+        model = arch_model(train, **GARCH_PARAMS)
+        model_fit = model.fit(disp='off')
+        pred = model_fit.forecast(horizon=1)
+        rolling_predictions.append(np.sqrt(pred.variance.values[-1, :][0]))
+    rolling_predictions = pd.Series(rolling_predictions, index=data.index[-test_size:])
+
+    ax.plot(test_data, color='blue', label='Testinės imties grąžos')
+    ax.plot(mdates.date2num(rolling_predictions.index.tolist()), np.sqrt(forecast.variance.values[-1, :]),
+            color='orange', label='Grąžos ilgalaikė prognozė')
+    ax.plot(mdates.date2num(rolling_predictions.index.tolist()), rolling_predictions, color='red',
+            label='Grąžų kasdienė prognozė')
+    ax.set_xlabel('Data')
+    ax.set_ylabel('Grąža')
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
     ax.tick_params(axis='y', labelcolor='blue')
 
-    ax2 = ax.twinx()
-    ax2.plot(mdates.date2num(test_data.index.tolist()), np.sqrt(forecast_vol), color='red', linestyle='--', label='Forecasted Volatility')
-    ax2.set_ylabel('Volatility', color='red')
-    ax2.xaxis.set_major_locator(mdates.YearLocator())
-    ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
-    ax2.tick_params(axis='y', labelcolor='red')
-
+    # PLOT 2: Forecasting future volatility
+    future_dates = pd.date_range(data.index[-1], periods=forecast_period + 1, freq='D')[1:]
+    # Plot actual log returns and forecasted data
+    ax2.plot(log_returns, color='blue', label='Tikrosios grąžos')
     # Plot forecasted log returns
-    future_dates = pd.date_range(test_data.index[-1], periods=forecast_period+1, freq='D')[1:]
-    ax2.plot(mdates.date2num(future_dates.tolist()), np.sqrt(future), color='green', linestyle=':', label='Forecasted Log Returns')
-    ax2.set_title(data.name)
-    ax2.set_xlabel('Date')
+    ax2.plot(mdates.date2num(future_dates.tolist()), future, color='green', linestyle=':',
+             label='Prognozuojamos grąžos')
+    ax2.set_xlabel('Data')
+    ax2.set_ylabel('Grąža')
     ax2.xaxis.set_major_locator(mdates.YearLocator())
     ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+    ax2.tick_params(axis='y', labelcolor='blue')
+    ax2.set_title(data.name)
+    ax2.set_xlim(pd.Timestamp("2024"), future_dates[-1])
 
     return aic, bic, backtest, error
 
 
+analyze_pacf()
+
 # Evaluate the GARCH model using log returns data
 rez = {}
 fig, axs = plt.subplots(3, 2, figsize=(16, 9))
+fig2, axs2 = plt.subplots(3, 2, figsize=(16, 9))
 
 # Loop over metals to generate subplots
-for m, ax in tqdm(zip(metals, axs.flatten())):
-    aic, bic, backtest, forecast_error = evaluate_model(m, df_all[m], ax)
+for m, ax, ax2 in tqdm(zip(metals, axs.flatten(), axs2.flatten())):
+    aic, bic, backtest, forecast_error = evaluate_model(df_all[m], ax, ax2)
     rez[m] = [aic, bic, backtest, forecast_error]
-    print(f'Metal: {m}\tAIC: {aic}\tBIC: {bic}\tBacktesting Result: {backtest}')
+    print(f'Metalas: {m}\tAIC: {aic}\tBIC: {bic}\tAtgalinio testavimo rezultatas: {backtest}')
 
-handle1, = ax.plot([], [], color='blue', label='Actual Log Returns')  # Empty plot for legend
-handle2, = ax.plot([], [], color='red', linestyle='--', label='Forecasted Volatility')  # Empty plot for legend
-handle3, = ax.plot([], [], color='green', linestyle=':',
-                   label='Future volatility')  # Empty plot for legend
-handles = [handle1, handle2, handle3]
-labels = ['Actual Log Returns', 'Forecasted Volatility', 'Forecasted Log Returns']
+handle0_1, = ax.plot([], [], color='blue', label='Testinės imties grąžos')  # Empty plot for legend
+handle0_2, = ax.plot([], [], color='orange', label='Grąžos ilgalaikė prognozė)')  # Empty plot for legend
+handle0_3, = ax.plot([], [], color='red', label='Grąžos kasdienė prognozė)')  # Empty plot for legend
 
-# Set the suptitle and manually add the legend
-fig.suptitle("GARCH Predictions with Forecast")
+handles = [handle0_1, handle0_2, handle0_3]
+labels = ['Testinės imties grąžos', 'Grąžos ilgalaikė prognozė',
+          'Grąžos kasdienė prognozė']
+fig.suptitle("GARCH prognozių tikrinimas")
 fig.legend(handles, labels, loc='upper right', ncol=4)
+fig.tight_layout()
+plt.savefig(os.path.join(BASE, 'GARCH', "garch_forecast.png"))
+plt.show()
 
-# Adjust layout and save figure
-fig.tight_layout()  # Adjust layout to avoid overlap with suptitle
-plt.savefig("pred/garch_forecast.png")
+handle1, = ax2.plot([], [], color='blue', label='Tikrosios grąžos')  # Empty plot for legend
+handle2, = ax2.plot([], [], color='green', linestyle=':', label='Prognozuojamos grąžos')  # Empty plot for legend
+
+handles = [handle1, handle2]
+labels = ['Tikrosios grąžos', 'Prognozuojamos grąžos']
+fig.suptitle("GARCH prognozavimas")
+fig.legend(handles, labels, loc='upper right', ncol=4)
+fig.tight_layout()
+plt.savefig(os.path.join(BASE, 'GARCH', "garch_forecast.png"))
 plt.show()
